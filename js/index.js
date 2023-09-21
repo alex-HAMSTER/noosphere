796fe44e3909e9373cdfe8dedc24a59252fbcacc
alex-HAMSTER <alex2003leong@gmail.com>
2023-09-22 00:49:32 +0300

    add logic in js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-alert("ok")
+/* alert("ok") */
+var name_id = document.getElementById('name_id');
+setInterval(
+    function(){
+        if(name_id.value == 0){
+            alert('name is clear')
+        }else{
+            alert('your name ' + name_id.value)
+        }
+    },4000
+);
+
+
+
+/* после нажатия на кнопку подтвердить,
+происходила проверка на значения внутри нейм_айди
+и если там пусто создавался блок красным шрифтом (поле пустое) */
+
+/* setInterval(
+    function(){
+        alert(name_id.value)
+    },4000
+) */
