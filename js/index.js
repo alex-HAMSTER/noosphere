6ee8e4162b98ef23800451eea3fa6ccc45a353f0
alex-HAMSTER <alex2003leong@gmail.com>
2023-09-22 22:37:50 +0300

    form pre-finish

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ setInterval(
 );
  */
 /* const name_id = document.getElementById('name_id');
-const btn_reg = document.getElementById('btn_reg');
+
 const errorMessage = document.getElementById('errorMessage')
 const input_class = document.querySelectorAll('input_class')
 
@@ -27,12 +27,14 @@ const inputElements = document.querySelectorAll('.input_class');
 const errorMessages = document.querySelectorAll('.errorMessage');
 const btnReg = document.getElementById('btn_reg');
 
+const form_login = document.getElementById('form_login')
+const form_reg = document.getElementById('form_reg')
 btnReg.addEventListener('click', function () {
     
     errorMessages.forEach(function (errorMessage) {
         errorMessage.textContent = '';
     });// Сброс ошибок перед проверкой
-
+    
     // Проверка каждого поля
     inputElements.forEach(function (inputElement, index) {
         const inputValue = inputElement.value.trim();
@@ -41,3 +43,28 @@ btnReg.addEventListener('click', function () {
         }
     });
 });
+
+const password_repeat = document.getElementById('password_repeat');
+const only_reg = document.querySelectorAll('.only_reg');
+const check_box = document.getElementById('check_box_login');
+const forgot_password = document.getElementById('forgot_password');
+const continue_google = document.getElementById('continue_google');
+
+form_login.addEventListener('click', function() {
+    only_reg.forEach(function(i){
+        i.style.cssText = 'display:none;'
+    })
+    check_box.style.cssText = 'display: block;'
+    btn_reg.style.cssText = 'margin-top: 27px;'
+    forgot_password.style.cssText = 'display: flex;'
+    continue_google.style.cssText = 'margin-top: 20px'
+})
+
+form_reg.addEventListener('click', function(){
+    only_reg.forEach(function(i){
+        i.style.cssText = 'display:block;'
+    })
+    check_box.style.cssText = 'display: none;'
+    btn_reg.style.cssText = 'margin-top: 37px;'
+    forgot_password.style.cssText = 'display: none;'
+})
