01974e2bebad9210b554647987bf2829649a4c73
alex-HAMSTER <alex2003leong@gmail.com>
2023-09-22 17:45:11 +0300

    error if input is clear

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /* alert("ok") */
-var name_id = document.getElementById('name_id');
+/* var name_id = document.getElementById('name_id');
 setInterval(
     function(){
         if(name_id.value == 0){
@@ -9,15 +9,35 @@ setInterval(
         }
     },4000
 );
+ */
+/* const name_id = document.getElementById('name_id');
+const btn_reg = document.getElementById('btn_reg');
+const errorMessage = document.getElementById('errorMessage')
+const input_class = document.querySelectorAll('input_class')
 
+btn_reg.addEventListener('click', function(){
+    const nameValue = input_class.value;
+    if(nameValue == ''){
+        errorMessage.textContent = 'Заповніть це поле';
+    }else{
+        errorMessage.textContent = '';
+    }
+}) */
+const inputElements = document.querySelectorAll('.input_class');
+const errorMessages = document.querySelectorAll('.errorMessage');
+const btnReg = document.getElementById('btn_reg');
 
+btnReg.addEventListener('click', function () {
+    
+    errorMessages.forEach(function (errorMessage) {
+        errorMessage.textContent = '';
+    });// Сброс ошибок перед проверкой
 
-/* после нажатия на кнопку подтвердить,
-происходила проверка на значения внутри нейм_айди
-и если там пусто создавался блок красным шрифтом (поле пустое) */
-
-/* setInterval(
-    function(){
-        alert(name_id.value)
-    },4000
-) */
+    // Проверка каждого поля
+    inputElements.forEach(function (inputElement, index) {
+        const inputValue = inputElement.value.trim();
+        if (inputValue === '') {
+            errorMessages[index].textContent = 'Заповніть це поле';
+        }
+    });
+});
