cece5d4cb22a2dfa4763af0ec745d71bb6ee9f35
alex-HAMSTER <alex2003leong@gmail.com>
2023-09-23 23:43:36 +0300

    add btn show password

--- js/index.js
+++ js/index.js
@@ -49,12 +49,15 @@ const only_reg = document.querySelectorAll('.only_reg');
 const check_box = document.getElementById('check_box_login');
 const forgot_password = document.getElementById('forgot_password');
 const continue_google = document.getElementById('continue_google');
+const chekPassword = document.getElementById('check_password_btn')
+const inpPassword = document.getElementById('inp_password')
 
 form_login.addEventListener('click', function() {
     only_reg.forEach(function(i){
         i.style.cssText = 'display:none;'
     })
-    check_box.style.cssText = 'display: block;'
+    chekPassword.style.cssText = 'display: flex;'
+    check_box.style.cssText = 'display: flex;'
     btn_reg.style.cssText = 'margin-top: 27px;'
     forgot_password.style.cssText = 'display: flex;'
     continue_google.style.cssText = 'margin-top: 20px'
@@ -64,7 +67,19 @@ form_reg.addEventListener('click', function(){
     only_reg.forEach(function(i){
         i.style.cssText = 'display:block;'
     })
+    chekPassword.style.cssText = 'display: none;'
     check_box.style.cssText = 'display: none;'
     btn_reg.style.cssText = 'margin-top: 37px;'
     forgot_password.style.cssText = 'display: none;'
 })
+
+
+
+chekPassword.addEventListener('click', function(){
+    inpPassword.type = 'text';
+})
+
+/* chekPassword.addEventListener('click', function(){
+    inpPassword.type = 'password';
+}) 
+логика которая вернёт его обратно*/
